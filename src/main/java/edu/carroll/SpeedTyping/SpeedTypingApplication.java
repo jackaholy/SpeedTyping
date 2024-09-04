209bb458a23baaa00065d9f606e48789a32191fb
Jack Holy <jholy@carroll.edu>
2024-09-04 11:27:25 -0600

    Initial commit

---
+++ src/main/java/edu/carroll/SpeedTyping/SpeedTypingApplication.java
@@ -0,0 +1,13 @@
+package edu.carroll.SpeedTyping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedTypingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedTypingApplication.class, args);
+	}
+
+}
