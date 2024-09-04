0ed155edc03cad4ff16b4c09e775d6999fab8581
Jack Holy <jholy@carroll.edu>
2024-09-04 11:46:12 -0600

    Create basic not-working web page

--- src/main/java/edu/carroll/SpeedTyping/SpeedTypingApplication.java
+++ src/main/java/edu/carroll/SpeedTyping/SpeedTypingApplication.java
@@ -1,13 +1,12 @@
 package edu.carroll.SpeedTyping;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
 public class SpeedTypingApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpeedTypingApplication.class, args);
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
 	}
-
 }
