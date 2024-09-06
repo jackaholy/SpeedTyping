e4f641f177df1ce1e8ef6645bc01f2e7a9288066
Jack Holy <jholy@carroll.edu>
2024-09-06 10:12:43 -0600

    Create a LoginController and move main method to correct location

--- src/main/java/edu/carroll/SpeedTyping/SpeedTypingApplication.java
+++ src/main/java/edu/carroll/SpeedTyping/SpeedTypingApplication.java
@@ -1,12 +1,13 @@
 package edu.carroll.SpeedTyping;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RestController
+@SpringBootApplication
 public class SpeedTypingApplication {
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedTypingApplication.class, args);
 	}
+
 }
