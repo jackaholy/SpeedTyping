a42716ec0e0fd960202d9133d4d7be005ede25f5
Jack Holy <jholy@carroll.edu>
2024-09-09 11:26:59 -0600

    Edit controller

--- src/main/java/edu/carroll/SpeedTyping/web/controller/MainController.java
+++ src/main/java/edu/carroll/SpeedTyping/web/controller/MainController.java
@@ -1,10 +1,10 @@
 package edu.carroll.SpeedTyping.web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class Controller {
+@Controller
+public class MainController {
     @GetMapping("/")
     public String index() {
         return "index";
