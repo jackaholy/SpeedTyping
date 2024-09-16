901383fc29565f16204a421eba190e2c48d78bf1
Jack Holy <jholy@carroll.edu>
2024-09-16 10:42:45 -0600

    Add proper mapping in controller

--- src/main/java/edu/carroll/SpeedTyping/web/controller/MainController.java
+++ src/main/java/edu/carroll/SpeedTyping/web/controller/MainController.java
@@ -5,8 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    // Maps the root URL to the home page.
     @GetMapping("/")
-    public String index() {
+    public String root() {
         return "home";
+    }
+
+    // Maps to the home page.
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    // Maps to the leaderboard page.
+    @GetMapping("/leaderboard")
+    public String leaderboard() {
+        return "leaderboard";
     }
 }
