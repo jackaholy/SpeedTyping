766a1e6ab74ef12f0df7001e900b8d8497f67488
Jack Holy <jholy@carroll.edu>
2024-10-02 11:03:24 -0600

    Add logging for switching pages

--- src/main/java/edu/carroll/SpeedTyping/web/controller/LeaderboardController.java
+++ src/main/java/edu/carroll/SpeedTyping/web/controller/LeaderboardController.java
@@ -3,6 +3,8 @@ package edu.carroll.SpeedTyping.web.controller;
 import edu.carroll.SpeedTyping.jpa.model.Score;
 import edu.carroll.SpeedTyping.service.LeaderboardService;
 import edu.carroll.SpeedTyping.service.LeaderboardServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @Controller
 public class LeaderboardController {
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
     private final LeaderboardService leaderboardService;
 
     public LeaderboardController(LeaderboardService leaderboardService) {
@@ -23,6 +26,7 @@ public class LeaderboardController {
     public String showLeaderboard(Model model) {
         List<Score> leaderboard = leaderboardService.getLeaderboard();
         model.addAttribute("leaderboard", leaderboard);
+        log.info("Switched to page: Leaderboard");
         return "leaderboard";
     }
 }
