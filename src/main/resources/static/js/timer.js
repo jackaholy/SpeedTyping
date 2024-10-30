5aeacafe911d3c3293421ace864f59559f26a649
Jack Holy <jholy@carroll.edu>
2024-10-29 18:27:02 -0600

    Add comments with old code, may not be helpful

--- src/main/resources/static/js/timer.js
+++ src/main/resources/static/js/timer.js
@@ -1,16 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let [milliseconds, seconds, minutes] = [0, 0, 0];
     let timeRef = document.querySelector(".timer-display");
     let interval = null;
 
     const targetContent = document.getElementById("targetContent");
     const userContent = document.getElementById("userContent");
+    // Thought about adding a totalTime const below to help grab the time in seconds.
+    // const totalTime = document.getElementById("totalTime");
 
     function displayTimer() {
         milliseconds += 10;
         if (milliseconds == 1000) {
             milliseconds = 0;
-            seconds++;
+            // ALSO thought about adding totalTime below.
+            // seconds++;
+            total_seconds++;
             if (seconds == 60) {
                 seconds = 0;
                 minutes++;
@@ -28,6 +32,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
         timeRef.innerHTML = `${m} : ${s} : ${ms}`;
     }
+
+    // MORE submitting the time functionality.
+    // function submitTime() {
+    //     // Set the total_seconds in the hidden input field
+    //     document.getElementById("total_seconds_input").value = totalTime;
+    //     document.getElementById("testForm").submit();
+    // }
+    // if (userContent.value === targetContent.value) {
+    //     submitTime();
+
+    //     (This may not need to be here)
+    //     clearInterval(interval);
+    //     interval = null;
+    //     totalTime.value = seconds;
 
     // Start timer when user begins typing
     userContent.addEventListener("input", () => {
