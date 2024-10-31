1a97a91f0de9c7ede135f22c567eda76ef80cf78
Jack Holy <jholy@carroll.edu>
2024-10-30 23:57:46 -0600

    Add isValid() tests

--- src/test/java/edu/carroll/SpeedTyping/jpa/model/ScoreTest.java
+++ src/test/java/edu/carroll/SpeedTyping/jpa/model/ScoreTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ScoreTest {
@@ -18,6 +18,34 @@ class ScoreTest {
         score = new Score("Test User", new Date(), 120.0);
         score.setPlayid(1L);
         score.setLevel(new Level());
+    }
+
+    @Test
+    public void testIsValid() {
+        assertTrue(score.isValid());
+    }
+
+    @Test
+    void testIsValid_AllFieldsNotNull() {
+        assertTrue(score.isValid(), "isValid() should return true when all fields are not null.");
+    }
+
+    @Test
+    void testIsValid_LevelIsNull() {
+        score = new Score(null, new Date(), 120.0);
+        assertFalse(score.isValid(), "isValid() should return false when level is null.");
+    }
+
+    @Test
+    void testIsValid_DateIsNull() {
+        score = new Score("1", null, 120.0);
+        assertFalse(score.isValid(), "isValid() should return false when date is null.");
+    }
+
+    @Test
+    void testIsValid_TimeIsNull() {
+        Score score = new Score("1", new Date(), null);
+        assertFalse(score.isValid(), "isValid() should return false when time is null.");
     }
 
     // Crazy path tests
